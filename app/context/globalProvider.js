66e58bf1f19153745b192c6270f663506962378b
acz19 <acz1992@hotmail.co.uk>
2024-03-02 11:37:20 +0100

    Add delete functionality

--- app/context/globalProvider.js
+++ app/context/globalProvider.js
@@ -2,11 +2,15 @@
 import React, { createContext, useState, useContext } from "react";
 import themes from "./themes";
 import axios from "axios";
+import { useUser } from "@clerk/nextjs";
+import toast from "react-hot-toast";
 
 export const GlobalContext = createContext();
 export const GlobalUpdateContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
+	const { user } = useUser();
+
 	const [selectedTheme, setSelectedTheme] = useState(0);
 	const [isLoading, setIsLoading] = useState(false);
 
@@ -25,15 +29,29 @@ export const GlobalProvider = ({ children }) => {
 		}
 	};
 
+	const deleteTask = async (id) => {
+		try {
+			const res = await axios.delete(`/api/tasks/${id}`);
+			toast.success("Task deleted");
+
+			allTasks();
+		} catch (error) {
+			console.log(error);
+			toast.error("Something went wrong");
+		}
+	};
+
 	React.useEffect(() => {
-		allTasks();
-	}, []);
+		if (user) allTasks();
+	}, [user]);
 
 	return (
 		<GlobalContext.Provider
 			value={{
 				theme,
 				tasks,
+				deleteTask,
+				isLoading,
 			}}
 		>
 			<GlobalUpdateContext.Provider value={{}}>
--- app/providers/ContextProvider.tsx
+++ app/providers/ContextProvider.tsx
@@ -17,8 +17,13 @@ const ContextProvider = ({ children }: Props) => {
 	}, []);
 
 	if (!isReady) {
-		return null;
+		return (
+			<div className="w-full h-full flex items-center justify-center">
+				<span className="loader"></span>
+			</div>
+		);
 	}
+
 	return (
 		<GlobalProvider>
 			<Toaster />
