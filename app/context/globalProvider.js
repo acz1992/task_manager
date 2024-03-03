5be5c376462554d9564ab6d70d345155eab356b4
acz19 <acz1992@hotmail.co.uk>
2024-03-03 16:25:56 +0100

    Insert completed task toggle function

--- app/context/globalProvider.js
+++ app/context/globalProvider.js
@@ -41,6 +41,18 @@ export const GlobalProvider = ({ children }) => {
 		}
 	};
 
+	const updateTask = async (task) => {
+		try {
+			const res = await axios.put(`/api/tasks`, task);
+
+			toast.success("Task updated");
+			allTasks();
+		} catch (error) {
+			console.log(error);
+			toast.error("Something went wrong");
+		}
+	};
+
 	const completedTasks = tasks.filter((task) => task.isCompleted === true);
 	const importantTasks = tasks.filter((task) => task.isImportant === true);
 	const incompleteTasks = tasks.filter((task) => task.isCompleted === false);
@@ -59,6 +71,7 @@ export const GlobalProvider = ({ children }) => {
 				completedTasks,
 				importantTasks,
 				incompleteTasks,
+				updateTask,
 			}}
 		>
 			<GlobalUpdateContext.Provider value={{}}>
